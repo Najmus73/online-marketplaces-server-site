c876e735353f5ae5d5b9303922fd4080e52b9a43
Najmus73 <shakibnajmus73@gmail.com>
2023-11-08 01:16:21 +0600

    Add Put method for update jobs by user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -41,6 +41,27 @@ async function run() {
       const newJob = req.body;
       console.log(newJob);
       const result = await jobsCollection.insertOne(newJob)
+      res.send(result);
+    })
+
+    app.put('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updateJobs = req.body;
+      const jobs = {
+        $set: {
+          image: updateJobs.image,
+          email: updateJobs.email,
+          jobTitle: updateJobs.jobTitle,
+          category: updateJobs.category,
+          deadline: updateJobs.deadline,
+          description: updateJobs.description,
+          minimumPrice: updateJobs.minimumPrice,
+          maximumPrice: updateJobs.maximumPrice
+        }
+      }
+      const result = await jobsCollection.updateOne(filter, jobs, options);
       res.send(result);
     })
 
